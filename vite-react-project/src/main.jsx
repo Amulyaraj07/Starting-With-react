af452c841ad5b98db05675ff5f3f9af45efdd17a
amulya <amulyaraj1234@gmail.com>
2024-09-16 19:24:45 +0530

    updated

--- vite-react-project/src/main.jsx
+++ vite-react-project/src/main.jsx
@@ -1,9 +1,39 @@
-import { StrictMode } from 'react'
+import React  from 'react'
+import ReactDOM from 'react-dom/client'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp(){
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  )
+}
+
+// const ReactElement={
+//   type: 'a',
+//   props: {
+//       href:'https://google.com',
+//       target:'_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
+
+const anotherUser="AmulyaRaj"
+
+const anotherElement=(
+  <a href='https://google.com' target='_blank'>Visit google</a>
 )
+
+const reactElement=React.createElement(
+  'a',
+  {href:'https://google.com',target:'_blank'},
+  'Click me to visit google',
+  anotherUser
+)
+
+createRoot(document.getElementById('root')).render(
+    // <App/>
+    reactElement
+);
